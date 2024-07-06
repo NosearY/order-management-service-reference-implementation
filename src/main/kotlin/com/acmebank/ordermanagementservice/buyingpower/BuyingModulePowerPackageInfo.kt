827dccf9c77e577919da041fca666542b321e081
NosearY <reason.yx@gmail.com>
2024-07-06 21:12:47 +0800

    Fix ArchUnit

--- src/main/kotlin/com/acmebank/ordermanagementservice/buyingpower/BuyingModulePowerPackageInfo.kt
+++ src/main/kotlin/com/acmebank/ordermanagementservice/buyingpower/BuyingModulePowerPackageInfo.kt
@@ -1,8 +1,6 @@
 package com.acmebank.ordermanagementservice.buyingpower
 
-import org.springframework.modulith.ApplicationModule
 import org.springframework.modulith.PackageInfo
 
 @PackageInfo
-@ApplicationModule(allowedDependencies = ["order", "corporateaction"])
 class BuyingModulePowerPackageInfo
--- src/main/kotlin/com/acmebank/ordermanagementservice/sellingpower/SellingModulePowerPackageInfo.kt
+++ src/main/kotlin/com/acmebank/ordermanagementservice/sellingpower/SellingModulePowerPackageInfo.kt
@@ -1,8 +1,6 @@
 package com.acmebank.ordermanagementservice.sellingpower
 
-import org.springframework.modulith.ApplicationModule
 import org.springframework.modulith.PackageInfo
 
 @PackageInfo
-@ApplicationModule(allowedDependencies = ["order", "corporateaction"])
 class SellingModulePowerPackageInfo
